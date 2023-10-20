00e257ddb38d9275a5dc7ef45729a9495a74921e
algosketch <algosketch@gmail.com>
2023-10-21 01:40:56 +0900

    feat : 액티비티 데이터 바인딩

--- app/src/main/java/me/algosketch/itunes/presentation/MainActivity.kt
+++ app/src/main/java/me/algosketch/itunes/presentation/MainActivity.kt
@@ -3,15 +3,20 @@ package me.algosketch.itunes.presentation
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
 import me.algosketch.itunes.R
+import me.algosketch.itunes.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
     }
 }
