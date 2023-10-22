5ceed0ec6a3bda269836ca7feca47c5e2a9ab0ea
algosketch <algosketch@gmail.com>
2023-10-22 19:08:29 +0900

    feat : 예외처리 로직 추가

--- app/src/main/java/me/algosketch/itunes/presentation/tracks/TracksActivity.kt
+++ app/src/main/java/me/algosketch/itunes/presentation/tracks/TracksActivity.kt
@@ -1,16 +1,22 @@
 package me.algosketch.itunes.presentation.tracks
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
+import androidx.paging.PagingData
 import androidx.paging.map
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import me.algosketch.itunes.R
 import me.algosketch.itunes.databinding.ActivityTracksBinding
+import me.algosketch.itunes.presentation.model.TrackModel
 import me.algosketch.itunes.presentation.model.toModel
 
 @AndroidEntryPoint
@@ -32,10 +38,26 @@ class TracksActivity : AppCompatActivity() {
             binding.rvTracks.adapter = withLoadStateFooter(ItLoadStateAdapter())
         }
 
+        adapter.addLoadStateListener { loadStates ->
+            val errorMessage = when {
+                loadStates.refresh is LoadState.Error -> (loadStates.refresh as LoadState.Error).error.message
+                loadStates.append is LoadState.Error -> (loadStates.append as LoadState.Error).error.message
+                else -> null
+            }
+
+            errorMessage?.let { message ->
+                showToast(message)
+            }
+        }
+
         lifecycleScope.launch {
             viewModel.trackFlow.collectLatest { pagingData ->
                 adapter.submitData(pagingData)
             }
         }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
